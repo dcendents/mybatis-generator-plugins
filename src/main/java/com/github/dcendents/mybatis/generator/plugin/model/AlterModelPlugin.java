31436b56e43cf4038b3a56bcd4e73f4014676cea
Daniel Beland <dcendents@gmail.com>
2015-05-25 23:17:21 -0400

    Adding AlterModelPlugin

---
+++ src/main/java/com/github/dcendents/mybatis/generator/plugin/model/AlterModelPlugin.java
@@ -0,0 +1,60 @@
+package com.github.dcendents.mybatis.generator.plugin.model;
+
+import java.util.List;
+
+import lombok.NoArgsConstructor;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+
+/**
+ * Mybatis generator plugin to modify the generated model.
+ */
+@NoArgsConstructor
+public class AlterModelPlugin extends PluginAdapter {
+	public static final String TABLE_NAME = "fullyQualifiedTableName";
+	public static final String ADD_INTERFACES = "addInterfaces";
+
+	private String tableName;
+	private String[] addInterfaces;
+
+	@Override
+	public boolean validate(List<String> warnings) {
+		tableName = properties.getProperty(TABLE_NAME);
+		String interfacesString = properties.getProperty(ADD_INTERFACES);
+
+		String warning = "Property %s not set for plugin %s";
+		if (!stringHasValue(tableName)) {
+			warnings.add(String.format(warning, TABLE_NAME, this.getClass().getSimpleName()));
+		}
+		if (!stringHasValue(interfacesString)) {
+			warnings.add(String.format(warning, ADD_INTERFACES, this.getClass().getSimpleName()));
+		} else {
+			addInterfaces = interfacesString.split(",");
+		}
+
+		return stringHasValue(tableName) && addInterfaces != null;
+	}
+
+	private boolean tableMatches(IntrospectedTable introspectedTable) {
+		return tableName.equals(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+	}
+
+	@Override
+	public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+		if (tableMatches(introspectedTable)) {
+			for (String theInterface : addInterfaces) {
+				FullyQualifiedJavaType type = new FullyQualifiedJavaType(theInterface);
+				topLevelClass.addImportedType(type);
+				topLevelClass.addSuperInterface(type);
+			}
+		}
+
+		return true;
+	}
+
+}
---
+++ src/test/java/com/github/dcendents/mybatis/generator/plugin/model/AlterModelPluginTest.java
@@ -0,0 +1,145 @@
+package com.github.dcendents.mybatis.generator.plugin.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+/**
+ * Tests for the class AlterModelPlugin.
+ */
+@RunWith(CdiRunner.class)
+public class AlterModelPluginTest {
+
+	private AlterModelPlugin plugin;
+
+	@Mock
+	private IntrospectedTable introspectedTable;
+	@Mock
+	private TopLevelClass topLevelClass;
+
+	private static final String TABLE_NAME = "table_name";
+
+	@Before
+	public void init() throws Exception {
+		plugin = new AlterModelPlugin();
+		plugin.getProperties().put(AlterModelPlugin.TABLE_NAME, TABLE_NAME);
+		plugin.getProperties().put(AlterModelPlugin.ADD_INTERFACES, Serializable.class.getName());
+		plugin.validate(new ArrayList<String>());
+	}
+
+	@Test
+	public void shouldBeInvalidWithoutAnyPropertyConfigured() {
+		// Given
+		AlterModelPlugin instance = new AlterModelPlugin();
+		
+		// When
+		List<String> warnings = new ArrayList<>();
+		boolean ok = instance.validate(warnings);
+
+		// Then
+		assertThat(ok).isFalse();
+		assertThat(warnings).hasSize(2);
+	}
+
+	@Test
+	public void shouldBeInvalidWithOnlyTheTableNameConfigured() {
+		// Given
+		AlterModelPlugin instance = new AlterModelPlugin();
+		instance.getProperties().put(AlterModelPlugin.TABLE_NAME, TABLE_NAME);
+
+		// When
+		List<String> warnings = new ArrayList<>();
+		boolean ok = instance.validate(warnings);
+
+		// Then
+		assertThat(ok).isFalse();
+		assertThat(warnings).hasSize(1);
+	}
+
+	@Test
+	public void shouldBeInvalidWithOnlyTheInterfacesConfigured() {
+		// Given
+		AlterModelPlugin instance = new AlterModelPlugin();
+		instance.getProperties().put(AlterModelPlugin.ADD_INTERFACES, Serializable.class.getName());
+
+		// When
+		List<String> warnings = new ArrayList<>();
+		boolean ok = instance.validate(warnings);
+
+		// Then
+		assertThat(ok).isFalse();
+		assertThat(warnings).hasSize(1);
+	}
+
+	@Test
+	public void shouldBeValidWhenBothPropertiesAreConfigured() {
+		// Given
+		AlterModelPlugin instance = new AlterModelPlugin();
+		instance.getProperties().put(AlterModelPlugin.TABLE_NAME, TABLE_NAME);
+		instance.getProperties().put(AlterModelPlugin.ADD_INTERFACES, Serializable.class.getName());
+
+		// When
+		List<String> warnings = new ArrayList<>();
+		boolean ok = instance.validate(warnings);
+
+		// Then
+		assertThat(ok).isTrue();
+		assertThat(warnings).isEmpty();
+	}
+	
+	@Test
+	public void shouldNotModifyModelBaseRecordClassIfTableDoesNotMatch() throws Exception {
+		// Given
+		given(introspectedTable.getFullyQualifiedTableNameAtRuntime()).willReturn("wrong_name");
+
+		// When
+		boolean ok = plugin.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+
+		// Then
+		assertThat(ok).isTrue();
+		verify(topLevelClass, times(0)).addField(any(Field.class));
+	}
+
+	@Test
+	public void shouldAddInterfacesToModelBaseRecordClass() throws Exception {
+		// Given
+		given(introspectedTable.getFullyQualifiedTableNameAtRuntime()).willReturn(TABLE_NAME);
+
+		// When
+		boolean ok = plugin.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+
+		// Then
+		assertThat(ok).isTrue();
+		
+		ArgumentCaptor<FullyQualifiedJavaType> typeCaptor = ArgumentCaptor.forClass(FullyQualifiedJavaType.class);
+		
+		verify(topLevelClass).addImportedType(typeCaptor.capture());
+		FullyQualifiedJavaType importedType = typeCaptor.getValue();
+		verify(topLevelClass).addSuperInterface(typeCaptor.capture());
+		FullyQualifiedJavaType interfaceType = typeCaptor.getValue();
+		
+		assertThat(importedType).isNotNull();
+		assertThat(interfaceType).isNotNull();
+		
+		assertThat(importedType).isSameAs(interfaceType);
+		assertThat(importedType.getFullyQualifiedName()).isEqualTo(Serializable.class.getName());
+	}
+
+}
